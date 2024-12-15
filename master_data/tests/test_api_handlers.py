6a09ed2511b64cb608b4ee5b63b41c2838abca28
Aya Aurora <aya.aurora25@gmail.com>
2024-12-15 19:07:20 +0000

    [10] Add vendor list handler with category and starting_price filter and name search

--- master_data/tests/test_api_handlers.py
+++ master_data/tests/test_api_handlers.py
@@ -5,6 +5,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 from master_data.models.vendor_category import VendorCategory
 from master_data.models.vendor import Vendor
+from master_data.models.vendor_package import VendorPackage
 
 
 def dummy_image_file(file_name, content_type):
@@ -23,12 +24,18 @@ def set_up_all_vendor_resources():
     cat = VendorCategory.objects.create(name='test_cat',
                                         description='test_desc',
                                         icon=uploaded)
-    Vendor.objects.create(name='test_vendor',
-                          description='test_desc',
-                          about='test_about',
-                          email='test@email.com',
-                          category=cat,
-                          profile_image=uploaded)
+    cat_2 = VendorCategory.objects.create(name='test_cat_2',
+                                          description='test_desc',
+                                          icon=uploaded)
+    vendor = Vendor.objects.create(name='test_vendor',
+                                   description='test_desc',
+                                   about='test_about',
+                                   email='test@email.com',
+                                   category=cat,
+                                   profile_image=uploaded)
+    VendorPackage.objects.create(name='test_package_2_1',
+                                 price=10,
+                                 vendor=vendor)
 
 
 class TestVendorHandler(APITestCase):
@@ -57,7 +64,113 @@ class TestVendorHandler(APITestCase):
         url = '/master_data/vendors/999/'
         response = self.client.get(url, format='json')
 
-        expected_vendor_data = [{'attr': None, 'code': 'not_found', 'detail': 'Not found.'}]
+        expected_error = [{'attr': None, 'code': 'not_found', 'detail': 'Not found.'}]
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        self.assertEqual(response.data['errors'], expected_vendor_data)
+        self.assertEqual(response.data['errors'], expected_error)
+
+    def test_get_vendor_list(self):
+        url = f'/master_data/vendors/'
+        response = self.client.get(url, format='json')
+        vendor = Vendor.objects.get(name='test_vendor')
+
+        expected_vendor_list = [
+            {
+                'id': vendor.id,
+                'name': vendor.name,
+                'profile_image': 'http://testserver' + vendor.profile_image.url,
+                'starting_price': 10
+            }
+        ]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'], expected_vendor_list)
+
+    def test_get_vendor_list_by_name_vendor_found(self):
+        url = f'/master_data/vendors/?search=test_vendor'
+        response = self.client.get(url, format='json')
+        vendor = Vendor.objects.get(name='test_vendor')
+
+        expected_vendor_list = [
+            {
+                'id': vendor.id,
+                'name': vendor.name,
+                'profile_image': 'http://testserver' + vendor.profile_image.url,
+                'starting_price': 10
+            }
+        ]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'], expected_vendor_list)
+
+    def test_get_vendor_list_by_name_vendor_not_found(self):
+        url = f'/master_data/vendors/?search=unknown'
+        response = self.client.get(url, format='json')
+
+        expected_vendor_list = []
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'], expected_vendor_list)
+
+    def test_get_vendor_list_by_category_vendor_found(self):
+        vendor = Vendor.objects.get(name='test_vendor')
+        url = f'/master_data/vendors/?category={vendor.category.id}'
+        response = self.client.get(url, format='json')
+
+        expected_vendor_list = [
+            {
+                'id': vendor.id,
+                'name': vendor.name,
+                'profile_image': 'http://testserver' + vendor.profile_image.url,
+                'starting_price': 10
+            }
+        ]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'], expected_vendor_list)
+
+    def test_get_vendor_list_by_category_vendor_not_found(self):
+        cat = VendorCategory.objects.get(name='test_cat_2')
+        url = f'/master_data/vendors/?category={cat.id}'
+        response = self.client.get(url, format='json')
+
+        expected_vendor_list = []
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'], expected_vendor_list)
+
+    def test_get_vendor_list_by_category_category_not_valid(self):
+        url = f'/master_data/vendors/?category=90'
+        response = self.client.get(url, format='json')
+
+        expected_error = [{'attr': 'category', 'code': 'invalid_choice', 'detail': 'Select a valid choice. That choice is not one of the available choices.'}]
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['errors'], expected_error)
+
+    def test_get_vendor_list_by_starting_price_vendor_found(self):
+        vendor = Vendor.objects.get(name='test_vendor')
+        url = f'/master_data/vendors/?starting_price={vendor.packages.first().price - 1}'
+        response = self.client.get(url, format='json')
+
+        expected_vendor_list = [
+            {
+                'id': vendor.id,
+                'name': vendor.name,
+                'profile_image': 'http://testserver' + vendor.profile_image.url,
+                'starting_price': 10
+            }
+        ]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'], expected_vendor_list)
+
+    def test_get_vendor_list_by_starting_price_vendor_not_found(self):
+        vendor = Vendor.objects.get(name='test_vendor')
+        url = f'/master_data/vendors/?starting_price={vendor.packages.first().price + 1}'
+        response = self.client.get(url, format='json')
+
+        expected_vendor_list = []
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'], expected_vendor_list)
