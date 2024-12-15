b90ec62af6f5f8f8e3724eb32124ac2885f7e9e8
Aya Aurora <aya.aurora25@gmail.com>
2024-12-15 19:07:20 +0000

    [10] Add vendor list handler with category and starting_price filter and name search

--- master_data/api_handlers/vendor_handler.py
+++ master_data/api_handlers/vendor_handler.py
@@ -1,20 +1,44 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
-from rest_framework import mixins, filters
+from django.db.models import Min
+from django_filters import rest_framework as filters
+from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 from backend.system_utility.custom_search_utility import CustomSearchFilter
+from backend.system_utility.pagination_utility import FiftyResultsPagination
 from backend.system_utility.settings_utility import DEFAULT_CACHE_TIME
 
 from master_data.models.vendor import Vendor
-from master_data.serializers.vendor_serializer import VendorSerializer
+from master_data.serializers.vendor_serializer import VendorSerializer, VendorListSerializer
 
 
-class VendorViewSet(mixins.RetrieveModelMixin, GenericViewSet):
+class VendorFilter(filters.FilterSet):
+    starting_price = filters.NumberFilter(field_name='packages__price', lookup_expr='gte')
 
-    queryset = Vendor.objects.all()
-    filter_backends = [filters.OrderingFilter, CustomSearchFilter]
+    class Meta:
+        model = Vendor
+        fields = {
+            'category': ['exact']
+        }
+
+
+@method_decorator(cache_page(DEFAULT_CACHE_TIME), name='list')
+class VendorViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet):
+
+    queryset = Vendor.objects.all().annotate(starting_price=Min('packages__price'))
+    pagination_class = FiftyResultsPagination
+    filter_backends = [filters.DjangoFilterBackend, CustomSearchFilter]
+    filterset_class = VendorFilter
     ordering_fields = ('id', 'name', 'created_at')
     ordering = ('-id',)
-    search_fields = ['name', 'category']
+    search_fields = ['name']
     serializer_class = VendorSerializer
+
+    def get_serializer_class(self):
+
+        if self.action in ['list']:
+
+            return VendorListSerializer
+
+        return VendorSerializer
